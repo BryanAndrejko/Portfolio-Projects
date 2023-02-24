a81ae04072322bb3597ac7ccbe412a299bc9e245
Bryan Andrejko <38671603+BryanAndrejko@users.noreply.github.com>
2023-02-24 12:13:04 -0700

    Commit first plot

--- customer_lifetime_value_CLV.R
+++ customer_lifetime_value_CLV.R
@@ -5,8 +5,11 @@ library(dplyr)
 library(readxl)
 
 #loading dataset as df
-df <- read_excel('~Online Retail.xlsx')
+df <- read_excel('~/Online Retail.xlsx')
 View(df)
 
 #view overall summary of df
 summary(df)
+
+# negative values and zero quantity
+plot(data = df, df$UnitPrice, df$Quantity)
